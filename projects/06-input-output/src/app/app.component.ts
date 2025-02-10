4da71681a8832fa38dfac412e7a313f9e39ad9d0
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-02-10 15:03:04 +0100

    solved ex 6

--- projects/06-input-output/src/app/app.component.ts
+++ projects/06-input-output/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Car } from './car';
+import { ListingComponent } from "./listing/listing.component";
 
 @Component({
   selector: 'app-root',
@@ -7,36 +8,18 @@ import { Car } from './car';
   template: `
     <h1>Saved Cars {{ savedCarList.length }}</h1>
     <section class="container">
-      <!-- This article element represents and entire listing -->
-      <article class="listing">
-        <div class="image-parent">
-          <img class="product-image" src="https://placehold.co/100x100" />
-        </div>
-        <section class="details">
-          <p class="title"><!-- car make and model--></p>
-          <hr />
-          <p class="detail">
-            <span>Year</span>
-            <span><!-- year --></span>
-          </p>
-          <div class="detail">
-            <span>Transmission</span>
-            <span><!-- transmission --></span>
-          </div>
-          <p class="detail">
-            <span>Mileage</span>
-            <span><!-- miles --></span>
-          </p>
-          <p class="detail">
-            <span>Price</span>
-            <span><!-- price --></span>
-          </p>
-        </section>
-      </article>
-      <!-- end car listing markup -->
+      @for (carEntry of carList; track carEntry) {
+        <app-listing [car]="carEntry" (carSaved)="addCarToSaved($event)" />
+      }
     </section>
+    @for (savedCardEntry of savedCarList; track savedCardEntry) {
+        <p>
+          {{ savedCardEntry.make }} {{ savedCardEntry.model }}
+        </p>
+      }
   `,
   styles: [],
+  imports: [ListingComponent],
 })
 export class AppComponent {
   savedCarList: Car[] = [];
@@ -74,4 +57,8 @@ export class AppComponent {
       transmission: 'Automatic',
     },
   ];
+
+  addCarToSaved(car: Car) {
+    this.savedCarList.push(car);
+  }
 }
