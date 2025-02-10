047cd9476b999fb953b1b6f66a8a455b2045c046
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-02-10 17:56:59 +0100

    solved ex 8

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: ` 
+    <h1>Welcome to {{ title }}!</h1> 
+    <router-outlet />
+    @for (product of productTitles; track product) {
+      <div class="listing">
+        <h2>{{ product }}</h2>
+        <a [routerLink]="['/details', $index]">View Details</a>
+      </div>
+    }
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
